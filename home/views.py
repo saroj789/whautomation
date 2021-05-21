99134865b2b177dd2f77a78dc496072f93b3d54b
saroj789 <sarojkumarvaishya@gmail.com>
2021-05-22 00:25:50 +0530

    3

--- home/views.py
+++ home/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import time
+import webbrowser as web
+import pyautogui as pg
+
 # Create your views here.
 
 def main(request):
     return render(request,"home/home.html")
 
 def WhatsappData(Ph,Message):
-    import time
-    import webbrowser as web
-    import pyautogui as pg
     Phone = "+91"+Ph
     web.open("https://web.whatsapp.com/send?phone="+Phone+"&text="+Message)
     time.sleep(30)
     pg.press("enter")
+    
 def SendData(request):
     if request.method == "POST":
         Ph = request.POST["Phone"]
